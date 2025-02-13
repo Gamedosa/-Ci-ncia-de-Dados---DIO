6ada15be65e0508f63f7c1a4ae92be8577f8e2ad
Gamedosa <gabriel20melo@gmail.com>
2025-02-13 18:30:04 -0300

    Operadores em python. Ainda em processo de adaptção da linguagem

---
+++ Operadores_Atribuição.py
@@ -0,0 +1,20 @@
+saldo = 500 
+print(saldo)
+
+saldo += 300
+print(saldo)
+
+saldo -= 100
+print(saldo)
+
+saldo *= 2
+print(saldo)
+
+saldo /= 20
+print(saldo)
+
+saldo //= 100
+print(saldo)
+
+saldo %= 1000
+print(saldo)
